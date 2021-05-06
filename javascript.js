133c721daf1b467a9a85a65dca2beb921cae6322
Roshan Gupta <gpta.duke.rohan.1219@gmail.com>
2021-05-06 12:30:38 +0530

    deleted old javascript file

--- javascript.js
+++ javascript.js
@@ -140,3 +140,28 @@ function deleteNotefunction(index) {
     localStorage.setItem("title", JSON.stringify(titleArray));
     showNotesfunction();
 }
+/*
+// search function for search bar 
+ let inputvalue = document.getElementById("search");
+inputvalue.addEventListener("input", searchfunction);
+let searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", searchfunction);
+
+function searchfunction() {
+    let inputvalue = document.getElementById("search");
+    let notecard = document.getElementsByClassName("notesCards");
+    Array.from(notecard).forEach(function(element) {
+        let cardText = element.getElementsByTagName("p")[0].innerHTML;
+        let cardTitle = element.getElementsByTagName("h5")[0].innerHTML;
+        if (cardText.includes(inputvalue)) {
+            element.style.display = "flex";
+        } else if (cardTitle.includes(inputvalue)) {
+            element.style.display = "flex";
+        } else {
+            element.style.display = "none";
+        }
+
+    });
+    let markup = document.getElementsByTagName
+}
+*/
